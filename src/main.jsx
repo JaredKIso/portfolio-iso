5902654d323044aa2d9500902efcd48afea7531d
Jared.Iso <88740718+JaredKIso@users.noreply.github.com>
2023-10-05 00:26:07 -0600

    God please it hurts, so many small changes Got the desktop to load finally. Updated the threejs version

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
+//import React from "react";
+//import ReactDOM from "react-dom";
+//import App from "./App"; // Assuming App.jsx is in the same directory as this file.
+//import "./index.css";
+//
+//ReactDOM.render(
+//  <React.StrictMode>
+//    <App />
+//  </React.StrictMode>,
+//  document.getElementById('root')
+//);
+
+
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App"; // Assuming App.jsx is in the same directory as this file.
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
+
